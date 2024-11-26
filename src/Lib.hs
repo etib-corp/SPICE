5b0d3dea176488bf2bb68c173ca699835af5f273
Neisan <nathan.maillot@epitech.eu>
2024-11-26 15:32:55 +0400

    chore: add documentation on Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,5 @@
+-- | This module is a utils library that provide many different type of functions.
+-- These functions can help us for the development on multiple kind of task.
 module Lib
     ( getInt, getDouble, isWhiteSpace
     ) where
@@ -5,19 +7,19 @@ module Lib
 import Data.Text (pack)
 import Data.Text.Read (double)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
+-- | Returns an integer extracted from the given string.
 getInt :: String -> Maybe Int
 getInt s = case reads s of
     [(n, "")] -> Just n
     _ -> Nothing
 
+-- | Returns a double extracted from the given string.
 getDouble :: String -> Maybe Double
 getDouble s = case double $ pack s of
    Right (d, _) -> Just d
    Left _ -> Nothing
 
+-- | Returns whether the char given is a whitespace or not.
 isWhiteSpace :: Char -> Bool
 isWhiteSpace ' ' = True
 isWhiteSpace '\n' = True
