774e018ba42fc4e377d8e81c2e36da9fb9484be2
Léo Sautron <leo.sautron@epitech.eu>
2024-11-26 23:08:42 +0400

    feat: add eval for var and define

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,6 +1,9 @@
 module Eval where
 
 import Structures
+import Control.Monad (foldM)  -- Import foldM to handle monads
+import System.Exit
+import Control.Applicative
 
 getExpr :: AST Expr -> Expr
 getExpr (Node (Integer i) []) = Integer i
@@ -15,12 +18,16 @@ getInEnv k (Env []) = Nothing
 getInEnv k (Env ((k', v):xs)) | k == k' = Just v
                               | otherwise = getInEnv k (Env xs)
 
-walker :: AST Expr -> Env -> Env
-walker Empty e = e
-walker (Node (Var a) []) e = case getInEnv (Var a) e of
-  Just v -> e
-  Nothing -> e
-walker (Node (Integer a) []) e = e
-walker (Node (Operator "=") (e1:e2:[])) e = pushEnv (getExpr e1) (getExpr e2) e
-walker (Node (List []) l) e = foldl (\acc x -> acc <> walker x acc) e l
+walker :: AST Expr -> Env -> IO Env
+walker (Node (Integer i) []) e = return e
+walker (Node (Float f) []) e = return e
+walker (Node (Var v) []) e = do
+  let v' = getInEnv (Var v) e
+  case v' of
+    Just v'' -> print v'' >> return e
+    Nothing -> putStrLn ("*** ERROR : variable " ++ v ++" is not bound.") >> return e
+walker (Node (Operator "=") (e1:e2:[])) env = return . pushEnv (getExpr e1) (getExpr e2) $ env
+walker (Node (List []) l) e = foldM (\acc x -> walker x acc >>= \newEnv -> return (acc <> newEnv)) e l
 
+eval :: AST Expr -> IO Env
+eval ast = walker ast (Env [])
--- src/Structures.hs
+++ src/Structures.hs
@@ -1,5 +1,9 @@
 module Structures where
 
+import Control.Monad
+import Control.Applicative
+import Control.Monad
+
 type Name = String
 
 data Expr
@@ -16,12 +20,23 @@ data Expr
   | ArithmeticOp Name Expr Expr -- Parsed
   | List [Expr]                 -- Parsed
   | If Expr Name Expr Expr      -- Parsed
---   | Call Name [Expr]
---   | Function Name [Name] Expr
---   | Extern Name [Name]
---   | ArithmeticOp Name Expr Expr
---   | UnaryOp Name Expr
-  deriving (Eq, Ord, Show)
+  -- | Call Name [Expr]
+  -- | Function Name [Name] Expr
+  -- | Extern Name [Name]
+  -- | ArithmeticOp Name Expr Expr
+  -- | UnaryOp Name Expr
+  deriving (Eq, Ord)
+
+instance Show Expr where
+  show (Integer i) = show i
+  show (Float f) = show f
+  show (Var v) = v
+  show (Operator o) = o
+  show (List l) = show l
+  show (ArithmeticOp o e1 e2) = show e1 ++ " " ++ show o ++ " " ++ show e2
+  show (Function n args e) = n ++ "(" ++ unwords args ++ ") = " ++ show e
+  show (If c t e1 e2) = "if " ++ show c ++ " then " ++ show t ++ " else " ++ show e1 ++ " end"
+  show (Callable n args) = n ++ "(" ++ unwords (map show args) ++ ")"
 
 data AST a = Empty | Node a [AST a]
 
