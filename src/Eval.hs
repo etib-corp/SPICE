b1953985a4a87316e6e70237fc10972180845234
Léo Sautron <leo.sautron@epitech.eu>
2024-12-02 18:51:37 +0400

    feat: add backend fucntion call and arithmetic op

--- src/Eval.hs
+++ src/Eval.hs
@@ -4,11 +4,15 @@ import Structures
 import Control.Monad (foldM)  -- Import foldM to handle monads
 import System.Exit
 import Control.Applicative
+import Ast
 
 getExpr :: AST Expr -> Expr
 getExpr (Node (Integer i) []) = Integer i
 getExpr (Node (Float f) []) = Float f
 getExpr (Node (Var v) []) = Var v
+getExpr (Node (Operator o) (e1:e2:[])) = ArithmeticOp o (getExpr e1) (getExpr e2)
+getExpr (Node (Declarator s) []) = Declarator s
+getExpr (Node (List l) []) = List (l)
 
 pushEnv :: Expr -> Expr -> Env -> Env
 pushEnv k v e = e <> Env [(k, v)]
@@ -17,6 +21,37 @@ getInEnv :: Expr -> Env -> Maybe Expr
 getInEnv k (Env []) = Nothing
 getInEnv k (Env ((k', v):xs)) | k == k' = Just v
                               | otherwise = getInEnv k (Env xs)
+
+baseOp :: Expr -> Expr -> (Expr -> Expr -> Expr) -> Env -> Maybe Expr
+baseOp e1 e2 op env = case (e1, e2) of
+  (Integer i1, Integer i2) -> Just (op e1 e2)
+  (Float f1, Float f2) -> Just (op e1 e2)
+  (Integer i, Float f) -> Just (op e1 e2)
+  (Float f, Integer i) -> Just (op e1 e2)
+  (Var v, Integer i) -> case getInEnv (Var v) env of
+    Just (Integer i') -> Just (op (Integer i') e2)
+    Just (Float f') -> Just (op (Float f') e2)
+    _ -> Nothing
+  (Integer i, Var v) -> case getInEnv (Var v) env of
+    Just (Integer i') -> Just (op e1 (Integer i'))
+    Just (Float f') -> Just (op e1 (Float f'))
+    _ -> Nothing
+  (Var v1, Var v2) -> case (getInEnv (Var v1) env, getInEnv (Var v2) env) of
+    (Just e1', Just e2') -> baseOp e1' e2' op env
+    (_, _) -> Nothing
+  (Var v, Float f) -> case getInEnv (Var v) env of
+    Just (Integer i') -> Just (op (Integer i') e2)
+    Just (Float f') -> Just (op (Float f') e2)
+    _ -> Nothing
+  (Float f, Var v) -> case getInEnv (Var v) env of
+    Just (Integer i') -> Just (op e1 (Integer i'))
+    Just (Float f') -> Just (op e1 (Float f'))
+    _ -> Nothing
+  _ -> Nothing
+
+createStack :: [Expr] -> [Expr] -> Env -> Env
+createStack [] [] env = env
+createStack (x:xs) (y:ys) env = createStack xs ys (pushEnv y x env)
 
 walker :: AST Expr -> Env -> IO Env
 walker (Node (Integer i) []) e = return e
@@ -27,7 +62,25 @@ walker (Node (Var v) []) e = do
     Just v'' -> print v'' >> return e
     Nothing -> putStrLn ("*** ERROR : variable " ++ v ++" is not bound.") >> return e
 walker (Node (Operator "=") (e1:e2:[])) env = return . pushEnv (getExpr e1) (getExpr e2) $ env
+walker (Node (Operator "+") (e1:e2:[])) env = print (baseOp (getExpr e1) (getExpr e2) (+) env) >> return env
+walker (Node (Operator "div") (e1:e2:[])) env = print (baseOp (getExpr e1) (getExpr e2) (/) env) >> return env
+walker (Node (Operator "-") (e1:e2:[])) env = print (baseOp (getExpr e1) (getExpr e2) (-) env) >> return env
+walker (Node (Operator "mod") (e1:e2:[])) env = print (baseOp (getExpr e1) (getExpr e2) mod env) >> return env
+walker (Node (Operator "*") (e1:e2:[])) env = print (baseOp (getExpr e1) (getExpr e2) (*) env) >> return env
 walker (Node (List []) l) e = foldM (\acc x -> walker x acc >>= \newEnv -> return (acc <> newEnv)) e l
+walker (Node (Declarator s) (var:exp:[])) e = return . pushEnv (Declarator s) (List [getExpr var, getExpr exp]) $ e
+walker (Node (Call s) [Node (List argNodes) []]) env =
+  case getInEnv (Declarator s) env of
+    Just (List [List vars, exp]) -> case length argNodes == length vars of
+      True -> execute exp (createStack argNodes vars env) >> return env
+      False -> putStrLn ("*** ERROR : function " ++ s ++ " expects " ++ show (length vars) ++
+                         " arguments, but " ++ show (length argNodes) ++ " were given.") >> return env
+    Nothing -> putStrLn ("*** ERROR : function " ++ s ++ " is not bound.") >> return env
+  where
+    -- Correct spelling and define `execute`
+    execute :: Expr -> Env -> IO Env
+    execute e env = walker (createAst e) env
+walker _ e = return e
 
 eval :: AST Expr -> IO Env
 eval ast = walker ast (Env [])
--- src/Structures.hs
+++ src/Structures.hs
@@ -3,6 +3,7 @@ module Structures where
 import Control.Monad
 import Control.Applicative
 import Control.Monad
+import Data.List (nubBy)
 
 type Name = String
 
@@ -27,6 +28,50 @@ data Expr
   -- | UnaryOp Name Expr
   deriving (Eq, Ord)
 
+instance Num Expr where
+  (Integer i) + (Integer j) = Integer (i + j)
+  (Float i) + (Float j) = Float (i + j)
+  (Integer i) + (Float j) = Float (fromIntegral i + j)
+  (Float i) + (Integer j) = Float (i + fromIntegral j)
+  (Integer i) - (Integer j) = Integer (i - j)
+  (Float i) - (Float j) = Float (i - j)
+  (Integer i) - (Float j) = Float (fromIntegral i - j)
+  (Float i) - (Integer j) = Float (i - fromIntegral j)
+  (Integer i) * (Integer j) = Integer (i * j)
+  (Float i) * (Float j) = Float (i * j)
+  (Integer i) * (Float j) = Float (fromIntegral i * j)
+  (Float i) * (Integer j) = Float (i * fromIntegral j)
+  abs (Integer i) = Integer (abs i)
+  abs (Float f) = Float (abs f)
+  signum (Integer i) = Integer (signum i)
+  signum (Float f) = Float (signum f)
+  fromInteger i = Integer (fromIntegral i)
+
+instance Fractional Expr where
+  (Integer i) / (Integer j) = Float (fromIntegral i / fromIntegral j)
+  (Float i) / (Float j) = Float (i / j)
+  (Integer i) / (Float j) = Float (fromIntegral i / j)
+  (Float i) / (Integer j) = Float (i / fromIntegral j)
+  fromRational r = Float (fromRational r)
+
+instance Enum Expr where
+  toEnum i = Integer i
+  fromEnum (Integer i) = i
+  fromEnum (Float f) = truncate f
+
+instance Real Expr where
+  toRational (Integer i) = toRational i
+  toRational (Float f) = toRational f
+
+instance Integral Expr where
+  toInteger (Integer i) = fromIntegral i
+  toInteger (Float f) = truncate f
+  quotRem (Integer i) (Integer j) = (Integer (i `quot` j), Integer (i `rem` j))
+  (Integer i) `mod` (Integer j) = Integer (i `mod` j)
+  (Integer i) `mod` (Float j) = Float (fromIntegral i `mod` j)
+  (Float i) `mod` (Integer j) = Float (i `mod` fromIntegral j)
+
+
 instance Show Expr where
   show (Integer i) = show i
   show (Float f) = show f
@@ -37,6 +82,7 @@ instance Show Expr where
   show (Function n args e) = n ++ "(" ++ unwords args ++ ") = " ++ show e
   show (If c t e1 e2) = "if " ++ show c ++ " then " ++ show t ++ " else " ++ show e1 ++ " end"
   show (Callable n args) = n ++ "(" ++ unwords (map show args) ++ ")"
+  show (Declarator n) = n
 
 data AST a = Empty | Node a [AST a]
 
@@ -52,5 +98,9 @@ instance Functor AST where
 data Env = Env { variables :: [(Expr, Expr)] } deriving (Show)
 
 instance Semigroup Env where
-  Env xs <> Env ys = Env $ xs ++ ys
+  Env xs <> Env ys = Env (removeDuplicates (ys ++ xs))
+    where
+      removeDuplicates = nubBy (\(k1, _) (k2, _) -> k1 == k2)
 
+instance Monoid Env where
+  mempty = Env []
