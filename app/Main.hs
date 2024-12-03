038bf9e6e82a5077a7fd4041c90353319864ad74
Léo Sautron <leo.sautron@epitech.eu>
2024-12-03 09:32:04 +0400

    feat: chose mode

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,12 @@ module Main (main) where
 import System.Environment
 import Lib
 import OptParser
+import Prompt
+
+choseMode :: Options -> IO ()
+choseMode (Options v f) = case length f of
+  0 -> performCLI defaultPrompt returnString
+  _ -> putStrLn "One mode selected"
 
 main :: IO ()
-main =  do
-  options <- getOptions
-  print options
-  putStrLn "Hello, Haskell!"
-  putStrLn "This is the main function of the app"
+main = choseMode =<< getOptions
