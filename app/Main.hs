f768ef2c836736eb179ba1c4eaab777607dffac5
Léo Sautron <leo.sautron@epitech.eu>
2024-12-03 09:53:44 +0400

    error: can't parse var

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,28 @@ import System.Environment
 import Lib
 import OptParser
 import Prompt
+import Files
+import Control.Applicative
+import System.Exit
+import LispParser
+import Structures
+import MyParser
+
+getFile :: [String] -> IO String
+getFile (x:[]) = secureGetContent x <|> error
+  where
+    error = exitWith (ExitFailure 84)
+
+parseFile :: String -> IO ()
+parseFile content = case parse content parseLispExpressionTest of
+  Left err -> putStrLn $ "Error: " ++ show err
+  Right expr -> putStrLn $ show expr
 
 choseMode :: Options -> IO ()
 choseMode (Options v f) = case length f of
   0 -> performCLI defaultPrompt returnString
-  _ -> putStrLn "One mode selected"
+  _ -> getFile f >>= parseFile
+
 
 main :: IO ()
 main = choseMode =<< getOptions
