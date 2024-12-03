b9834ad033ab62c01e3b20d2c5b3029cb97636c1
Neisan <nathan.maillot@epitech.eu>
2024-12-03 14:42:09 +0400

    fix: parser for specific instructions

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,16 @@ import System.Environment
 import Lib
 import OptParser
 import Prompt
+import LispParser
+import Structures
+
+compiler :: String -> Env -> IO ()
+compiler str env = parse str parseLispExpressionTest
+
 
 choseMode :: Options -> IO ()
 choseMode (Options v f) = case length f of
-  0 -> performCLI defaultPrompt returnString
+  0 -> performCLI defaultPrompt emptyEnv compiler
   _ -> putStrLn "One mode selected"
 
 main :: IO ()
