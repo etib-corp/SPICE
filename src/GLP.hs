93facdf6c45bbbdcd0cb52dec5bd0ef24185d454
Neisan <nathan.maillot@epitech.eu>
2025-01-08 11:52:19 +0400

    fix: merge all operators together

--- src/GLP.hs
+++ src/GLP.hs
@@ -25,10 +25,10 @@ parseBooleanConfig = do
     pure $ createBooleanParser boolValues
 
 createOperatorParser :: [String] -> Parser Expr
-createOperatorParser (op:"expression":"expression":[]) = 
-    ArithmeticOp <$> 
+createOperatorParser (op:"expression":"expression":[]) =
+    ArithmeticOp <$>
     parseGivenString (unquote op)
-    <*> (parseWhiteSpaces *> parseExpression) 
+    <*> (parseWhiteSpaces *> parseExpression)
     <*> (parseWhiteSpaces *> parseExpression)
 createOperatorParser ("expression":op:"expression":[]) = do
     leftExpr <- parseExpression
@@ -42,57 +42,27 @@ createOperatorParser ("expression":"expression":op:[]) = do
     return $ ArithmeticOp name leftExpr rightExpr
 createOperatorParser _ = fail "Invalid Operator configuration: expected 3 elements defined (operator, right expression, left expression)."
 
-
-parsePlusConfig :: Parser (Parser Expr)
-parsePlusConfig = do
-    parseGivenString "plus:" *> parseWhiteSpaces
+parseOperatorConfig :: Parser (Parser Expr)
+parseOperatorConfig = do
+    (parseDeclarator) *> parseWhiteSpaces
     plusValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
     pure $ createOperatorParser plusValue
-
-parseMinusConfig :: Parser (Parser Expr)
-parseMinusConfig = do
-    parseGivenString "minus:" *> parseWhiteSpaces
-    minusValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
-    pure $ createOperatorParser minusValue
-
-parseMultiplyConfig :: Parser (Parser Expr)
-parseMultiplyConfig = do
-    parseGivenString "multiply:" *> parseWhiteSpaces
-    multiplyValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
-    pure $ createOperatorParser multiplyValue
-
-parseDivideConfig :: Parser (Parser Expr)
-parseDivideConfig = do
-    parseGivenString "divide:" *> parseWhiteSpaces
-    divideValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
-    pure $ createOperatorParser divideValue
-
-parseModuloConfig :: Parser (Parser Expr)
-parseModuloConfig = do
-    parseGivenString "modulo:" *> parseWhiteSpaces
-    moduloValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
-    pure $ createOperatorParser moduloValue
-
-parseEqConfig :: Parser (Parser Expr)
-parseEqConfig = do
-    parseGivenString "equal:" *> parseWhiteSpaces
-    eqValue <- parseSepBy (parseString <|> parseString') (parseWhiteSpaces *> parseGivenString "->" <* parseWhiteSpaces)
-    pure $ createOperatorParser eqValue
+        where
+            parseDeclarator = parseGivenString "plus:" <|> parseGivenString "minus:" <|> parseGivenString "multiply:" <|> parseGivenString "divide:" <|> parseGivenString "modulo:" <|> parseGivenString "equal:"
 
 getConfig :: String -> ParserConfig
 getConfig config = ParserConfig {
     parseBoolean' = case parse config parseBooleanConfig of
         Left _ -> fail "Failed to parse boolean configuration."
         Right parser -> parser,
-    parserOperator = case parse config parsePlusConfig of
+    parserOperator = case parse config parseOperatorConfig of
         Left _ -> fail "Failed to parse operator configuration."
         Right parser -> parser
 }
 
 parseConfigTest :: IO ()
-parseConfigTest = case parse "plus: \"+\" -> expression -> expression" parsePlusConfig of
+parseConfigTest = case parse "minus: expression -> \"-\" -> expression" parseOperatorConfig of
     Left err -> putStrLn $ show err
-    Right pa -> case parse "+ 1 2" pa of
+    Right pa -> case parse "1 - 2" pa of
         Left err -> putStrLn $ show err
         Right result -> putStrLn $ show result
-
