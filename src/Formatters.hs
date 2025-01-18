07752e3d3c8d268fdee8a4e004a66eeb7ad9f472
Neisan <nathan.maillot@epitech.eu>
2025-01-18 11:35:51 +0400

    refactor: migrate utils and formatters functions in separated modules

---
+++ src/Formatters.hs
@@ -0,0 +1,30 @@
+module Formatters where
+
+import MyParser
+
+import Control.Applicative
+
+import Data.Functor
+
+type Formatter = (String, String)
+
+parsePrefixConfig :: Parser String
+parsePrefixConfig = (parseGivenString "prefix:" *> parseWhiteSpaces *> parseStringInQuotes) <|> parseGivenString ""
+
+parseSuffixConfig :: Parser String
+parseSuffixConfig = (parseGivenString "suffix:" *> parseWhiteSpaces *> parseStringInQuotes) <|> parseGivenString ""
+
+parseEmptyFormatter :: Parser Formatter
+parseEmptyFormatter = parseGivenString ":" $> ("","")
+
+parseFullFormatters :: Parser Formatter
+parseFullFormatters = do
+    parseGivenString "{" *> parseWhiteSpaces
+    prefix <- parsePrefixConfig
+    ((parseWhiteSpaces *> parseGivenString ",") <|> parseGivenString "") *> parseWhiteSpaces
+    suffix <- parseSuffixConfig
+    parseGivenString "}"
+    pure $ (prefix, suffix)
+
+parseFormatters :: Parser Formatter
+parseFormatters = parseFullFormatters <|> parseEmptyFormatter
---
+++ src/GLPUtils.hs
@@ -0,0 +1,30 @@
+module GLPUtils where
+
+import MyParser
+import Structures
+
+import Control.Applicative
+
+import Data.Functor
+
+unquote :: String -> String
+unquote [] = ""
+unquote str = init $ tail str
+
+parseConfigString :: Parser String
+parseConfigString = parseString <|> parseString'
+
+useOps :: [Parser Expr] -> Parser Expr
+useOps (x:xs) = x <|> useOps xs
+useOps [] = fail "empty list..."
+
+loopedParser :: [String] -> Parser String
+loopedParser [] = fail "Can not parse any elements of given table."
+loopedParser (x:xs) = parseGivenString x <|> loopedParser xs
+
+parseTable :: Parser String
+parseTable = parseGivenString "[" *> parseSomeUntil (parseAnyChar) (parseChar ']')
+
+parseTable' :: String -> Either Error [String]
+parseTable' tab = parse tab (parseSepBy parseStringInQuotes (parseGivenString "," *> parseWhiteSpaces) <* parseWhiteSpaces)
+
